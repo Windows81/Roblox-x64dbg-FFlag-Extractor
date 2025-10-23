27ddd6adeb4855f0cb2c8f7be0dbdd5aa1e4557d
Windows81 <nastyahmede@gmail.com>
2025-10-23 17:18:07 +0000

    2025-10-24T0018Z

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This tool requires the use of `x64dbg_automate`, which in turn requires `x64dbg` to be in your system path.
 
+All addresses are stored as RVAs (relative virtual addresses) to account for ASLR (address-space randomisation). In fact, x64dbg's patch format `.1337` also uses RVAs.
+
 Here's an example run:
 
 ```ps
@@ -10,6 +12,23 @@ python main.py "c:\Users\USER\Projects\FilteringDisabled\Roblox\v463\Studio\Robl
 
 This program is optimised for different versions of Rōblox Studio. Bitness shouldn't be a problem.
 
-## JSON Test Files
+## JSON "Test" Files
 
-Attached are some flag dumps. [`./test-studio-v347.json`](./test-studio-v347.json) (32-bit), [`./test-studio-v410.json`](./test-studio-v410.json) (64-bit), and [`./test-studio-v463.json`](./test-studio-v463.json) (64-bit) were both generated with the `-df` and `-ds` flags. [`./test-studio-v548.json`](./test-studio-v548.json) and [`./test-studio-v695.json`](./test-studio-v695.json) only used `-df` because string extraction is much harder to do therewith.
+Attached are some flag dumps.
+
+[`./test-studio-v347.json`](./test-studio-v347.json) (32-bit), [`./test-studio-v410.json`](./test-studio-v410.json) (64-bit), and [`./test-studio-v463.json`](./test-studio-v463.json) (64-bit) were both generated with the `-df` and `-ds` flags.
+
+Generations for [`./test-studio-v548.json`](./test-studio-v548.json) and [`./test-studio-v695.json`](./test-studio-v695.json) only used `-df` because string extraction is much harder to do therewith.
+
+I generate these files on **my machine** using the following command:
+
+```ps1
+python main.py "C:\Users\USER\Downloads\0.347.0.28462 (version-d9cf1f7e4fe14aa9)\RobloxStudioBeta.exe" -df -ds > "./test/v347-studio.json" &&
+python main.py "C:\Users\USER\Projects\FilteringDisabled\Roblox\v348\Server\RCCService.exe" -df -ds > "./test/v348-server.json" &&
+python main.py "C:\Users\USER\Downloads\0.410.1.33582 (version-ea2cea5d307b4fe6)\RobloxStudioBeta.exe" -df -ds > "./test/v410-studio.json" &&
+python main.py "C:\Users\USER\Projects\FilteringDisabled\Roblox\v463\Studio\_RobloxStudioBeta-ORIGINAL.exe" -df -ds > "./test/v463-studio.json" &&
+python main.py "C:\Users\USER\Projects\FilteringDisabled\Roblox\v463\Player\RobloxPlayerBeta.exe" -df -ds > "./test/v463-player.json" &&
+python main.py "C:\Users\USER\Projects\FilteringDisabled\Roblox\v463\Server\RCCService.exe" -df -ds > "./test/v463-server.json" &&
+python main.py "F:\Users\USERNAME\AppData\Local\RobloxStudio2022\RobloxStudioBeta.exe" -df > "./test/v548-studio.json" &&
+python main.py "C:\Users\USER\AppData\Local\Roblox Studio\RobloxStudioBeta.exe" -df > "./test/v695-studio.json"
+```
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def eprint(*args, **kwargs):
 
 class flag_type(enum.Enum):
     FFlag = b'LockViolationInstanceCrash'
-    FInt = b'DataStoreJobFrequencyInSeconds'  # b'StreamingSafeMemWatermarkMB'
+    FInt = b'StreamingSafeMemWatermarkMB'  # b'DataStoreJobFrequencyInSeconds'
     FString = b'FriendsOnlineUrl'  # b'RobloxAnalyticsURL'
     FLog = b'GfxClustersFull'
 
@@ -50,7 +50,13 @@ def is_exe_64_bit(path: str) -> bool:
             return False
 
 
-def get_memory_ref(ins: Instruction):
+def get_memory_ptr(ins: Instruction):
+    '''
+    The x86 language gives you a variety of ways of reading or writing data to a particular address.
+    Rōblox has been known to use many different instruction types to grab the same data for the same purpose.
+    For example, Studio v347 uses `push` to add the name of an FFlag as an argument for the next `call`.
+    However, Studio v463 uses `lea` into a register `rcx` for the exact same purpose.
+    '''
     match ins.instruction.split(' ', 1)[0]:
 
         case 'lea' | 'mov' | 'push' | 'xmm' | 'movaps' | 'movups':
@@ -101,14 +107,15 @@ def iterate_ref(session: X64DbgClient, addr_to_find: int, fr: int, sz: int, max_
 
 
 @functools.cache
-def find_word_in_memory(session: X64DbgClient, word: bytes):
+def find_string_in_memory(session: X64DbgClient, s: bytes):
     '''
-    Finds a complete string `word` and returns the address.
+    Finds a *complete* string and returns the address.
+    Throws if that word does not exist.
     '''
     region_map = organise_map(session)
     assert session.cmd_sync('find %x,"%s"' % (
         region_map['.rdata'].base_address,
-        (b'\0' + word + b'\0').hex(),
+        (b'\0' + s + b'\0').hex(),
     ))
     (word_base, _) = session.eval_sync('$result')
     assert word_base > 0
@@ -129,25 +136,41 @@ def find_lea_from_addr(session: X64DbgClient, memory_addr: int):
 
     lea_ins = session.disassemble_at(lea_addr)
     assert lea_ins is not None
-    lea_ref = get_memory_ref(lea_ins)
-    assert lea_ref is not None
-    return (lea_ref, lea_addr)
+    lea_ptr = get_memory_ptr(lea_ins)
+    assert lea_ptr is not None
+    return (lea_ptr, lea_addr)
 
 
 @functools.cache
 def find_mem_lea_from_name_lea(session: X64DbgClient, lea_addr: int):
+    '''
+    Definitions that I made up:
+    - `name_lea`: the memory-read instruction with an address to the *name* of the flag.
+    Can be `lea`, `push`, `mov`, etc.
+    - `mem_lea`: the memory-read instruction with an address to the *memory location* of that same flag.
+    Can be `lea`, `push`, `mov`, etc.
+
+    This function takes advantage of the fact that:
+    - `mem_lea` directly precedes `name_lea`.
+    - `mem_lea` and `name_lea` both have the same instruction size.
+
+    This has been validated in various Studio versions from 347 up to 695.
+    '''
     lea_ins = session.disassemble_at(lea_addr)
     assert lea_ins is not None
     offset = -lea_ins.instr_size
 
     ins = session.disassemble_at(lea_addr+offset)
     assert ins is not None
-    ref = get_memory_ref(ins)
+    ref = get_memory_ptr(ins)
     assert ref is not None
     return (offset, ref)
 
 
 def read_string(session: X64DbgClient, addr: int, can_start_in_middle: bool = False):
+    '''
+    Reads a string beginning at a specific memory address.
+    '''
     if can_start_in_middle:
         addr += session.read_memory(addr - 0x100, 0x100).rfind(b'\0') - 0xff
     (strlen, _) = session.eval_sync('strlen(utf8((%x))' % addr)
@@ -158,6 +181,10 @@ def read_string(session: X64DbgClient, addr: int, can_start_in_middle: bool = Fa
 
 @functools.cache
 def trace_down_until_branches(session: X64DbgClient, base_addr: int, skip_current: bool = True, count: int = 1):
+    '''
+    Generates a list of branch statements which follow `base_addr`.
+    Limited by `count`, which if set to -1, can run up to the next `int3` instruction
+    '''
     offset = 0
     while True:
         ins = session.disassemble_at(base_addr + offset)
@@ -196,8 +223,8 @@ def process_call_stuff(session: X64DbgClient, call_arg: int, load_offset: int, o
             orig_ins = session.disassemble_at(addr - orig_offset)
             assert orig_ins is not None
 
-            load_ref = get_memory_ref(load_ins)
-            orig_ref = get_memory_ref(orig_ins)
+            load_ref = get_memory_ptr(load_ins)
+            orig_ref = get_memory_ptr(orig_ins)
             if load_ref is None or orig_ref is None:
                 continue
             yield (load_ref, orig_ref)
@@ -207,11 +234,11 @@ def process_call_stuff(session: X64DbgClient, call_arg: int, load_offset: int, o
 
 @functools.cache
 def get_string_load_places(session: X64DbgClient):
-    flag_name_mem_addr = find_word_in_memory(session, b'FriendsOnlineUrl')
+    flag_name_mem_addr = find_string_in_memory(session, b'FriendsOnlineUrl')
     (_, flag_name_addr) = find_lea_from_addr(session, flag_name_mem_addr)
     (_, flag_load_ref) = find_mem_lea_from_name_lea(session, flag_name_addr)
 
-    default_val_mem_addr = find_word_in_memory(session, b'/my/friendsonline')
+    default_val_mem_addr = find_string_in_memory(session, b'/my/friendsonline')
     (_, default_val_addr) = find_lea_from_addr(session, default_val_mem_addr)
 
     lea_addr = min(
@@ -272,6 +299,12 @@ def read_value(session: X64DbgClient, flag_t: flag_type, name_addr: int):
             assert False
 
 
+def to_rva(session: X64DbgClient, addr: int):
+    (result, success) = session.eval_sync('mod.rva(%x)' % addr)
+    assert success
+    return result
+
+
 @functools.cache
 def find_addrs_into_branch(session: X64DbgClient, func_addr: int, lea_offset: int):
     region_map = organise_map(session)
@@ -288,30 +321,41 @@ def find_addrs_into_branch(session: X64DbgClient, func_addr: int, lea_offset: in
             lea_ins = session.disassemble_at(lea_addr)
             assert lea_ins is not None
 
-            lea_ref = get_memory_ref(lea_ins)
+            lea_ref = get_memory_ptr(lea_ins)
             if lea_ref is None:
                 continue
-            yield (lea_ref, lea_addr)
+            yield (lea_addr, lea_ref)
 
     return list(gen())
 
 
 def process_of_type(session: X64DbgClient, flag_t: flag_type):
-    flag_name_addr = find_word_in_memory(session, flag_t.value)
+    flag_name_addr = find_string_in_memory(session, flag_t.value)
     eprint(
-        '[%s] template flag `%s` found at %X' %
-        (flag_t.name, flag_t.value.decode(), flag_name_addr)
+        '[%s] template flag `%s` found at :$%X' %
+        (
+            flag_t.name,
+            flag_t.value.decode(),
+            to_rva(session, flag_name_addr),
+        )
     )
     (_, lea_addr) = find_lea_from_addr(session, flag_name_addr)
     eprint(
-        '[%s] lea for flag name found at %X' %
-        (flag_t.name, lea_addr)
+        '[%s] lea for flag name found at :$%X' %
+        (
+            flag_t.name,
+            to_rva(session, lea_addr),
+        )
     )
     (call_offset, call_ins) = next(trace_down_until_branches(session, lea_addr))
     call_addr = call_ins.arg[0].constant
     eprint(
-        '[%s] jmp/call found from %X to %X' %
-        (flag_t.name, lea_addr + call_offset, call_addr)
+        '[%s] jmp/call found from :$%X to :$%X' %
+        (
+            flag_t.name,
+            to_rva(session, lea_addr + call_offset),
+            to_rva(session, call_addr),
+        )
     )
     flag_name_addrs = find_addrs_into_branch(session, call_addr, call_offset)
     eprint(
@@ -319,8 +363,8 @@ def process_of_type(session: X64DbgClient, flag_t: flag_type):
         (flag_t.name, len(flag_name_addrs))
     )
     flag_names = (
-        (read_string(session, r) or b'').decode()
-        for (r, a) in flag_name_addrs
+        (read_string(session, ptr) or b'').decode()
+        for (_, ptr) in flag_name_addrs
     )
     return zip(flag_names, flag_name_addrs)
 
@@ -333,12 +377,17 @@ def process(session: X64DbgClient, extract_default_flags: bool, extract_default_
             else:
                 extract = extract_default_flags
 
-            for name, (ref, addr) in process_of_type(session, flag_t):
-                json_val = (f'ref: %X; addr: %X' % (ref, addr),)
+            for name, (addr, ptr) in process_of_type(session, flag_t):
+                addr_str = 'addr = :$%X; ptr = :$%X' % (
+                    to_rva(session, addr),
+                    to_rva(session, ptr),
+                )
+                json_val = (addr_str,)
 
                 result = None
                 if extract:
                     result = read_value(session, flag_t, addr)
+
                 if result is not None:
                     json_val = (*json_val, result)
 
